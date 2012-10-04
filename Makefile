b177a8495492da6b9b69645270b8bd56d4211a25
Cédric Darné <cedric.darne@gmail.com>
2012-10-04 23:44:02 +0200

    Added first exercises

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 include Makefile.inc
 DIRS=ex19
-EXE=ex14 ex15 ex16 ex16b ex17 ex18
+EXE=ex1 ex3 ex4 ex5 ex6 ex7 ex8 ex9 ex10 ex11 ex12 ex13 ex14 ex15 ex16 ex16b ex17 ex18
 
 .PHONY: all clean ex19
 
@@ -8,6 +8,7 @@ all: $(EXE) $(DIRS)
 
 clean:
 	rm -f $(EXE) *.dat
+	rm -Rf *.dSYM
 	make clean -C ex19
 #	for d in $(DIRS); do $(MAKE) clean -C $d; done
 
---
+++ ex12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    if(argc <= 2) {
+        printf("You only have one argument. You suck.\n");
+    } else if(argc > 2 && argc < 5) {
+        printf("Here's your arguments:\n");
+
+        for(i = 1; i < argc;  i++) {
+            printf("%s ", argv[i]);
+        }
+        printf("\n");
+    } else {
+        printf("You have too many arguments. You suck.\n");
+    }
+
+    return 0;
+}
